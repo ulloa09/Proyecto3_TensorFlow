b5c719d7c86dc11805c8bfb7c1163bd065ad2c33
Mauricio Martínez Ulloa <mauricio.martinezu@iteso.mx>
2025-10-18 11:07:32 -0600

    Limpieza

--- README.md
+++
@@ -1,3 +0,0 @@
-# Proyecto3_TensorFlow
-
-Systematic trading strategy using deep learning models trained on engineered time series features.
--- claseinicial.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import mlflow
-
-# Load an preprocess the CIFAR-10 dataset
-
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
-
-print("x_train shape:", x_train.shape)
