11ab711a01bc0bf99e010546374917e9b533f144
Mauricio Martínez Ulloa <mauricio.martinezu@iteso.mx>
2025-10-23 14:26:52 -0600

    Conteo de datos después de DROP NA

--- signals.py
+++ signals.py
@@ -94,6 +94,8 @@ def generate_features(df: pd.DataFrame) -> pd.DataFrame:
     print(f"Total de Datos previo a drop: {len(df)}")
     df = df.dropna()
 
+    print(f"Datos NA después de Drop:\n{df.isna().sum()}")
+    print(f"Total de Datos después de drop: {len(df)}")
     return df
 
 
