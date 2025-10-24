f8e0a857e6697c47cb57cb1a57b560e444eaed34
Mauricio Martínez Ulloa <mauricio.martinezu@iteso.mx>
2025-10-24 16:42:34 -0600

    Definición inicial de datos en backtest.py y rename

--- backtest.py
+++ backtest.py
@@ -3,113 +3,36 @@ import numpy as np
 import pandas as pd
 import ta
 
-from functions import rsi_signals, macd_signals, bbands_signals, obv_signals, atr_breakout_signals, adx_signals
 from metrics import annualized_sharpe, annualized_calmar, annualized_sortino, win_rate
 from models import Operation, get_portfolio_value
 
 
-def backtest(data, trial, params=None) -> float:
+def backtest(data, stop_loss:float, take_profit:float, n_shares:float) -> float:
     # --- Preparación inicial del DataFrame ---
     # Copia el DataFrame para evitar modificar el original.
-    # Convierte la columna 'timestamp' a formato datetime y establece el índice temporal.
     data = data.copy()
-    data['Datetime'] = pd.to_datetime(data['timestamp'], unit= 'ms', errors='coerce')
-    data.set_index('Datetime')
-
-    # --- Definición de parámetros de trading ---
-    # Si se recibe un trial de Optuna, se sugieren valores para los parámetros de la estrategia.
-    # Si se reciben parámetros directamente, se usan esos valores.
-    # Estos parámetros controlan los umbrales para indicadores técnicos, gestión de riesgo y tamaño de posición.
-    if trial is not None:
-        # --- cuando Optuna optimiza ---
-        stop_loss = trial.suggest_float('stop_loss', 0.02, 0.05)
-        take_profit = trial.suggest_float('take_profit', 0.04, 0.15)
-        rsi_window = trial.suggest_int('rsi_window', 10, 30)
-        rsi_lower = trial.suggest_int('rsi_lower', 25, 35)
-        rsi_upper = trial.suggest_int('rsi_upper', 65, 75)
-        macd_fast = trial.suggest_int('macd_fast', 5, 12)
-        macd_slow = trial.suggest_int('macd_slow', 20, 40)  # debe ser > fast
-        macd_signal = trial.suggest_int('macd_signal', 9, 18)
-        bb_window = trial.suggest_int('bb_window', 20, 50)
-        bb_std = trial.suggest_int('bb_std', 1, 3)
-        obv_window = trial.suggest_int('obv_window', 20, 50)
-        atr_window = trial.suggest_int('atr_window', 10, 30)
-        atr_mult = trial.suggest_float('atr_mult', 1, 2.5)
-        adx_window = trial.suggest_int('adx_window', 10, 30)
-        adx_tresh = trial.suggest_int('adx_tresh', 20, 30)
-        n_shares = trial.suggest_float('n_shares', 0.5, 5)
-    elif params is not None:
-        # --- cuando se usa con best_params ---
-        stop_loss = params['stop_loss']
-        take_profit = params['take_profit']
-        rsi_window = params['rsi_window']
-        rsi_lower = params['rsi_lower']
-        rsi_upper = params['rsi_upper']
-        macd_fast = params['macd_fast']
-        macd_slow = params['macd_slow']
-        macd_signal = params['macd_signal']
-        bb_window = params['bb_window']
-        bb_std = params['bb_std']
-        obv_window = params['obv_window']
-        atr_window = params['atr_window']
-        atr_mult = params['atr_mult']
-        adx_window = params['adx_window']
-        adx_tresh = params['adx_tresh']
-        n_shares = params['n_shares']
-    else:
-        # En caso de no recibir ni trial ni parámetros, se lanza un error.
-        raise ValueError("Debes pasar un trial de Optuna o un diccionario params.")
-
-    # --- Cálculo de señales técnicas ---
-    # Se obtienen señales de compra y venta basadas en diferentes indicadores técnicos.
-    # Cada función devuelve series booleanas indicando cuándo se activa cada señal.
-    buy_rsi, sell_rsi = rsi_signals(data, rsi_window=rsi_window, rsi_lower=rsi_lower, rsi_upper=rsi_upper)
-    buy_macd, sell_macd = macd_signals(data, fast=macd_fast, slow=macd_slow, signal=macd_signal)
-    buy_bbands, sell_bbands = bbands_signals(data, bb_window, bb_std)
-    buy_obv, sell_obv = obv_signals(data, window=obv_window)
-    buy_atr, sell_atr = atr_breakout_signals(data, atr_window=atr_window, atr_mult=atr_mult)
-    buy_adx, sell_adx = adx_signals(data, window=adx_window, threshold=adx_tresh)
-
-    # --- Combinación de señales ---
-    # Se concatenan las señales de compra y venta para contar cuántas se activan simultáneamente.
-    # La estrategia requiere al menos 2 señales de compra o venta para generar una señal definitiva.
-    buy_df = pd.concat([
-                        buy_rsi,
-                        buy_macd,
-                        buy_bbands,
-                        buy_obv,
-                        buy_atr,
-                        buy_adx,
-    ], axis=1)
-    sell_df = pd.concat([
-                         sell_rsi,
-                         #sell_macd,
-                         sell_bbands,
-                         #sell_obv,
-                         #sell_atr,
-                         #sell_adx
-    ], axis=1)
-
-    # Condición: al menos 2 señales activas para confirmar compra o venta
-    buy_signal = (buy_df.sum(axis=1) >= 2)
-    sell_signal = (sell_df.sum(axis=1) >= 2)
 
     # --- Preparación del DataFrame con señales ---
-    # Se limpia el DataFrame para eliminar filas con datos faltantes y se añaden columnas con las señales generadas.
     historic = data.copy()
-    historic['buy_signal'] = buy_signal
-    historic['sell_signal'] = sell_signal
+    historic = historic.dropna()
+
 
     # --- Inicialización de variables para la simulación ---
     # COM representa el costo por operación (comisión).
     # SL y TP son los niveles de stop loss y take profit.
     # cash es el capital inicial disponible.
     COM = 0.125 / 100
+    BORROW_RATE = 0.25 / 100
     SL = stop_loss
     TP = take_profit
 
+    days = 252
+    BORROW_DIARIO = BORROW_RATE / days
+
     cash = 1_000_000
 
+    '''
+    
     # Listas para mantener las posiciones abiertas de tipo LONG y SHORT.
     active_long_positions: list[Operation] = []
     active_short_positions: list[Operation] = []
@@ -221,3 +144,5 @@ def backtest(data, trial, params=None) -> float:
         return calmar
     else:
         return calmar, values_port, results
+
+'''
